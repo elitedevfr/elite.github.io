5541bf78ba1134ddf6201bd200b326bc275e6039
Elite <144380243+elitedevfr@users.noreply.github.com>
2023-09-10 00:14:56 +0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,25 @@
+const projects = document.getElementById("hiprojects")
+const contact = document.getElementById("hicontact")
+const bio = document.getElementById("hibio")
+
+
+
+
+
+projects.addEventListener('click', function() {
+    window.location.href = 'pages/projects.html';
+  });
+
+  contact.addEventListener('click', function() {
+    window.location.href = 'pages/contact.html';
+  });
+
+
+  bio.addEventListener('click', function() {
+    window.location.href = 'pages/bio.html';
+  });
+
+
+  
+  
+  
